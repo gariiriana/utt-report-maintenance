04cc3e3c39f78de4ac1757e3bd753b7f28a06081
Gari Iriana <gariiriana@gmail.com>
2026-07-31 14:54:49 +0700

    fix: Add DOM removeChild & insertBefore polyfill to prevent React DOM reconciliation errors

--- frontend/main.tsx
+++ frontend/main.tsx
@@ -6,5 +6,30 @@ import { createRoot } from "react-dom/client";
 
   registerSW({ immediate: true });
 
-  createRoot(document.getElementById("root")!).render(<App />);
+  // Polyfill to prevent React DOM removeChild/insertBefore crashes caused by browser extensions or Framer Motion DOM mutations
+if (typeof window !== 'undefined') {
+  const originalRemoveChild = Node.prototype.removeChild;
+  Node.prototype.removeChild = function <T extends Node>(child: T): T {
+    if (child.parentNode !== this) {
+      if (child.parentNode) {
+        return child.parentNode.removeChild(child);
+      }
+      return child;
+    }
+    return originalRemoveChild.call(this, child) as T;
+  };
+
+  const originalInsertBefore = Node.prototype.insertBefore;
+  Node.prototype.insertBefore = function <T extends Node>(newNode: T, referenceNode: Node | null): T {
+    if (referenceNode && referenceNode.parentNode !== this) {
+      if (referenceNode.parentNode) {
+        return referenceNode.parentNode.insertBefore(newNode, referenceNode);
+      }
+      return newNode;
+    }
+    return originalInsertBefore.call(this, newNode, referenceNode) as T;
+  };
+}
+
+createRoot(document.getElementById("root")!).render(<App />);
 
